10dd18546e08b8a04cbbf55d66daef2f939db11b
kevf-s <73202338+kevf-s@users.noreply.github.com>
2021-06-30 15:06:49 -0500

    Add files via upload

---
+++ Unidad 3/main.c
@@ -0,0 +1,6 @@
+#include "funcion_control.c"
+
+void main(int argc,char *argv[])
+{
+	control (argv[1],argv[2]);
+}
---
+++ Unidad 3/ordenar.c
@@ -0,0 +1,9 @@
+int comparar(const void *a, const void *b){
+const char *cp1 = a, *cp2 = b;
+
+    for (; toupper(*cp1) == toupper(*cp2); cp1++, cp2++)
+        if (*cp1 == '\0')
+            return 0;
+    return ((toupper(*cp1) < toupper(*cp2)) ? -1 : +1);
+	
+} 
